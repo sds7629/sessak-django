ee0dc39f6edfe32b6db3286e571129a5bf9ec99a
sds7629 <sds7629@naver.com>
2024-03-06 23:28:51 +0900

    new

--- sessak/settings.py
+++ sessak/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.apps.AppsConfig",
-    "apis.apps.ApisConfig",
+    "apps.users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
