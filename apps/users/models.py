82cb3793b831ab598ccbf8db50d4860e1e5552ab
sds7629 <sds7629@naver.com>
2024-03-07 13:04:11 +0900

    new cast

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,59 @@
+from typing import Any, Optional, cast
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(
+        self,
+        name: str,
+        email: str,
+        age: int,
+        password: Optional[str] = None,
+        **kwargs: Any,
+    ) -> "User":
+        if not email:
+            raise ValueError("필수")
+        user: "User" = cast("User", self.model(name=name, email=email, age=age))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        name: str,
+        email: str,
+        age: int,
+        password: Optional[str] = None,
+        **extra_fields: Any,
+    ) -> "User":
+        user: "User" = self._create_user(name=name, email=email, age=age, password=password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    name = models.CharField(max_length=10)
+    email = models.EmailField(verbose_name="email address", unique=True, max_length=50)
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "age"]
+
+    class Meta:
+        db_table = "users"
+
+    def is_staff(self) -> bool:
+        return self.is_superuser
--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS: list[str] = []
 
-
+AUTH_USER_MODEL = "users.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
